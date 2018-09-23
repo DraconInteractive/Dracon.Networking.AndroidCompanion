3cdf6170d1dd61c9771a1d91b135ac340f1c895f
RarCeth <rarceth@gmail.com>
2018-09-23 14:09:20 +0800

    sdr

---
+++ Assets/NetworkClientUI.cs
@@ -0,0 +1,59 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Networking.NetworkSystem;
+
+
+public class NetworkClientUI : MonoBehaviour {
+
+	NetworkClient client;
+
+	string ipenter = "";
+	void OnGUI () {
+		string ipaddress = LocalIPAddress();
+		GUI.Box(new Rect(10, Screen.height - 50, 100, 50), ipaddress);
+		GUI.Label(new Rect(20, Screen.height - 30, 100, 20), "Status: " + client.isConnected);
+
+		if (!client.isConnected) {
+			ipenter = GUI.TextField(new Rect(50, Screen.height * 0.1f, 120, 50), ipenter);
+			if (GUI.Button(new Rect(50, Screen.height * 0.15f, 120, 100), "Connect")) {
+				Connect();
+			}
+		}
+
+		if (ipenter != "") {
+			GUI.Label(new Rect(Screen.width * 0.5f, Screen.height * 0.5f, 250, 100), ipenter);
+		}
+	}
+	// Use this for initialization
+	void Start () {
+		client = new NetworkClient();
+	}
+	
+	void Connect () {
+		client.Connect(ipenter, 25000);
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public string LocalIPAddress()
+     {
+         IPHostEntry host;
+         string localIP = "";
+         host = Dns.GetHostEntry(Dns.GetHostName());
+         foreach (IPAddress ip in host.AddressList)
+         {
+             if (ip.AddressFamily == AddressFamily.InterNetwork)
+             {
+                 localIP = ip.ToString();
+                 break;
+             }
+         }
+         return localIP;
+     }
+}
---
+++ Assets/NetworkServerUI.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Networking.NetworkSystem;
+using UnityStandardAssets.CrossPlatformInput;
+
+
+public class NetworkServerUI : MonoBehaviour {
+
+	string ipaddress = "";
+	void OnGUI () {
+		
+		GUI.Box (new Rect(10, Screen.height - 50, 100, 50), ipaddress);
+		GUI.Label(new Rect(20, Screen.height - 35, 100, 20), "Status: " + NetworkServer.active);
+		GUI.Label(new Rect(20, Screen.height - 20, 100, 20), "Connected: " + NetworkServer.connections.Count);
+	}
+
+	void Start () {
+		ipaddress = LocalIPAddress();
+		NetworkServer.Listen(25000);
+	}
+
+	public string LocalIPAddress()
+     {
+         IPHostEntry host;
+         string localIP = "";
+         host = Dns.GetHostEntry(Dns.GetHostName());
+         foreach (IPAddress ip in host.AddressList)
+         {
+             if (ip.AddressFamily == AddressFamily.InterNetwork)
+             {
+                 localIP = ip.ToString();
+                 //break;
+             }
+			 print ("IP: " + ip.ToString());
+         }
+         return localIP;
+     }
+}
